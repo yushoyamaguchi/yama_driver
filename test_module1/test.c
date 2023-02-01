fb8173b2b1756417d1059ebf633cb2e498b87df9
yushoyamaguchi <ysh.824@outlook.jp>
2023-02-01 17:42:57 +0900

    first kernel module

---
+++ test_module1/test.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+
+static int test_init(void)
+{
+    printk("Hello my module\n");
+    return 0;
+}
+
+static void test_exit(void)
+{
+    printk("Bye bye my module\n");
+}
+
+module_init(test_init);
+module_exit(test_exit);
