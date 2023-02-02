442b05af34f6797a0d2c8c12bd914baa079b158d
yushoyamaguchi <ysh.824@outlook.jp>
2023-02-02 13:41:42 +0900

    netdev.c 0202

--- yama_e1000e/netdev.c
+++ yama_e1000e/netdev.c
@@ -9,21 +9,50 @@
 #include <asm/current.h>
 #include <asm/uaccess.h>
 
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
 #include <linux/pci.h>
 
 MODULE_LICENSE("GPL v2");
 
-char e1000e_driver_name[] = "yama_e1000e";
+char yama_e1000e_driver_name[] = "yama_e1000e";
 
-static int e1000_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
+struct yama_e1000e_adaptor{
+
+};
+
+static int yama_e1000e_netdev_open(struct net_device *dev){
+	return 0;
+}
+
+static int yama_e1000e_netdev_close(struct net_device *dev){
+	return 0;
+}
+
+
+static netdev_tx_t yama_e1000e_start_xmit(struct sk_buff *skb, struct net_device *dev){
+	return NETDEV_TX_OK;
+}
+
+static const struct net_device_ops yama_e1000e_netdev_ops = {
+	.ndo_open = yama_e1000e_netdev_open,
+	.ndo_stop = yama_e1000e_netdev_close,
+	.ndo_start_xmit = yama_e1000e_start_xmit,
+};
+
+static int yama_e1000_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 {
+	struct net_device *netdev;
+	netdev = alloc_etherdev(sizeof(struct yama_e1000e_adaptor));
+	netdev->netdev_ops=&yama_e1000e_netdev_ops;
+	register_netdev(netdev);
     return 0;
 }
 
 /* PCI Device API Driver */
-static struct pci_driver e1000_driver = {
-	.name     = e1000e_driver_name,
-	.probe    = e1000_probe
+static struct pci_driver yama_e1000_driver = {
+	.name     = yama_e1000e_driver_name,
+	.probe    = yama_e1000_probe
 };
 
 
@@ -33,12 +62,12 @@ static struct pci_driver e1000_driver = {
  * e1000_init_module is the first routine called when the driver is
  * loaded. All it does is register with the PCI subsystem.
  **/
-static int __init e1000_init_module(void)
+static int __init yama_e1000_init_module(void)
 {
 
-	return pci_register_driver(&e1000_driver);
+	return pci_register_driver(&yama_e1000_driver);
 }
-module_init(e1000_init_module);
+module_init(yama_e1000_init_module);
 
 /**
  * e1000_exit_module - Driver Exit Cleanup Routine
@@ -46,8 +75,8 @@ module_init(e1000_init_module);
  * e1000_exit_module is called just before the driver is removed
  * from memory.
  **/
-static void __exit e1000_exit_module(void)
+static void __exit yama_e1000_exit_module(void)
 {
-	pci_unregister_driver(&e1000_driver);
+	pci_unregister_driver(&yama_e1000_driver);
 }
-module_exit(e1000_exit_module);
+module_exit(yama_e1000_exit_module);
