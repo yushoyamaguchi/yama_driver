f4c92e4830d240f73e90fb7c2fafa53e04ba8843
yushoyamaguchi <ysh.824@outlook.jp>
2023-02-22 14:29:04 +0900

    prepare of pci recognization 1

--- yama_e1000e/netdev.c
+++ yama_e1000e/netdev.c
@@ -13,6 +13,9 @@
 #include <linux/etherdevice.h>
 #include <linux/pci.h>
 
+#include "include/pci_table.h"
+#include "include/e1000.h"
+
 MODULE_LICENSE("GPL v2");
 
 char yama_e1000e_driver_name[] = "yama_e1000e";
@@ -21,11 +24,11 @@ struct yama_e1000e_adaptor{
 
 };
 
-static int yama_e1000e_netdev_open(struct net_device *dev){
+int yama_e1000e_netdev_open(struct net_device *dev){
 	return 0;
 }
 
-static int yama_e1000e_netdev_close(struct net_device *dev){
+int yama_e1000e_netdev_close(struct net_device *dev){
 	return 0;
 }
 
@@ -36,7 +39,7 @@ static netdev_tx_t yama_e1000e_start_xmit(struct sk_buff *skb, struct net_device
 
 static const struct net_device_ops yama_e1000e_netdev_ops = {
 	.ndo_open = yama_e1000e_netdev_open,
-	.ndo_stop = yama_e1000e_netdev_close,
+	.ndo_stop = yama_e1000e_netdev_close, //扱えるハードウェアのリスト
 	.ndo_start_xmit = yama_e1000e_start_xmit,
 };
 
@@ -52,6 +55,7 @@ static int yama_e1000_probe(struct pci_dev *pdev, const struct pci_device_id *en
 /* PCI Device API Driver */
 static struct pci_driver yama_e1000_driver = {
 	.name     = yama_e1000e_driver_name,
+	.id_table	  = yama_e1000_pci_tbl,
 	.probe    = yama_e1000_probe
 };
 
